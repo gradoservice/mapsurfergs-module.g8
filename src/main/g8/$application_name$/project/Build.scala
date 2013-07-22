bd66dcd9a1bbd0be39d7f178d337dc48a09c08ad
perelmanjulia <san13greal@gmail.com>
2013-07-22 03:47:46 -0700

    Update Build.scala
    new version of geoportal

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
 		javaCore,
 		javaJdbc,
 		javaEbean,
-		"com.gradoservice" %% "geoportal" % "3.1.3"	  
+		"com.gradoservice" %% "geoportal" % "3.1.7"	  
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
